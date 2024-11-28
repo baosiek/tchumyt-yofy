c9c7fb2dae087fe27a3d311ed12c1b39870a1d97
Bruno Adam Osiek <baosiek@gmail.com>
2024-11-28 08:55:55 -0800

    Documentation updated

--- llm/llm/__init__.py
+++ llm/llm/__init__.py
@@ -21,6 +21,10 @@ test_config: str = "llm/configs/gpt_config_124m_test.yaml"
 prod_config: str = "llm/configs/gpt_config_124m.yaml"
 file_path: str = ""
 
+# Priority for test configuration.
+# In case it is found, loads it. Otherwise tries to 
+# load the regular one. If not found either, then
+# raises file not found exception.
 if os.path.exists(test_config):
     file_path = test_config
 elif os.path.exists(prod_config):
