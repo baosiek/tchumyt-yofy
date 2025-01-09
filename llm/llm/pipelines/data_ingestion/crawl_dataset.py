8117228a84c68656f7291e13a4a58f1a14b649af
Bruno Adam Osiek <baosiek@gmail.com>
2025-01-09 12:57:18 -0800

    dataset developed

---
+++ llm/llm/pipelines/data_ingestion/crawl_dataset.py
@@ -0,0 +1,18 @@
+from torch import tensor
+from torch.utils.data import Dataset
+from typing import List
+
+from llm.utils.tchumyt_mongo_client import TchumytMongoClient
+
+
+class CrawlDataset(Dataset):
+    def __init__(self, client: TchumytMongoClient, query: str):
+        super().__init__()
+        self.client: TchumytMongoClient = client
+        self.records: List[str] = list(client.query(query))
+
+    def __len__(self) -> int:
+        return len(self.records)
+
+    def __getitem__(self, index):
+        return tensor(self.records[index]['x']), tensor(self.records[index]['y'])
---
+++ llm/tests/pipelines/data_ingestion/test_dataset.py
@@ -0,0 +1,33 @@
+import pytest
+
+from typing import Any, Dict
+from bson.objectid import ObjectId
+
+from llm.utils.tchumyt_mongo_client import TchumytMongoClient
+from llm.llm.pipelines.data_ingestion.crawl_dataset import CrawlDataset
+
+
+@pytest.fixture
+def pathname() -> str:
+    return "llm/configs/dataset_config.yaml"
+
+
+@pytest.fixture
+def query() -> Dict[str, Any]:
+    return {"_id": ObjectId("678001d24570296256278926")}
+
+
+def test_dateset_len(pathname: str, query: Dict[str, Any]):
+    client: TchumytMongoClient = TchumytMongoClient(pathname)
+    dataset: CrawlDataset = CrawlDataset(client=client, query=query)
+
+    assert dataset.__len__() == 1
+
+
+def test_dataset_getItem(pathname: str, query: Dict[str, Any]):
+    client: TchumytMongoClient = TchumytMongoClient(pathname)
+    dataset: CrawlDataset = CrawlDataset(client=client, query=query)
+    x, y = dataset.__getitem__(0)
+
+    assert len(x) == 256
+    assert len(y) == 256
