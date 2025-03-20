379cee95039e94b16880dcab578abea70d640c15
Bruno Adam Osiek <baosiek@gmail.com>
2025-03-20 10:38:40 -0700

    tmyts implemented

---
+++ llm/llm/architecture/tmyts/min_gru.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear, Identity, Module
+
+'''
+Code from:
+@inproceedings{Feng2024WereRA,
+    title   = {Were RNNs All We Needed?},
+    author  = {Leo Feng and Frederick Tung and Mohamed Osama Ahmed and Yoshua
+    Bengio and Hossein Hajimirsadegh},
+    year    = {2024},
+    url     = {https://api.semanticscholar.org/CorpusID:273025630}
+}
+'''
+
+
+class minGRU(Module):
+    def __init__(
+            self,
+            dim: int,
+            expansion_factor: float = 1.0,
+            proj_out: float = None
+    ):
+        super().__init__()
+
+        dim_inner = int(dim * expansion_factor)
+        proj_out = default(proj_out, expansion_factor != 1.0)
+
+        self.to_hidden_and_gate: Linear = Linear(
+            dim,
+            dim_inner * 2,
+            bias=False
+        )
+
+        self.to_out: Linear = Linear(dim_inner, dim, bias=False) \
+            if proj_out else Identity()
+
+    def forward(
+            self,
+            x: torch.Tensor,
+            prev_hidden: torch.Tensor = None,
+            return_next_prev_hidden: torch.Tensor = False
+    ):
+        seq_len: int = x.shape[1]
+        hidden, gate = self.to_hidden_and_gate(x).chunk(2, dim=-1)
+
+        if seq_len == 1:
+            # handle sequential
+
+            hidden: torch.Tensor = g(hidden)
+            gate = gate.sigmoid()
+            out = torch.lerp(prev_hidden, hidden, gate) \
+                if exists(prev_hidden) else (hidden * gate)
+        else:
+            # parallel
+
+            log_coefficients = -F.softplus(gate)
+
+            log_z = -F.softplus(-gate)
+            log_tilde_h = log_g(hidden)
+            log_values = log_z + log_tilde_h
+
+            if exists(prev_hidden):
+                log_values = torch.cat((prev_hidden.log(), log_values), dim=1)
+                log_coefficients = F.pad(log_coefficients, (0, 0, 1, 0))
+
+            out = heinsen_associative_scan_log(log_coefficients, log_values)
+            out = out[:, -seq_len:]
+
+        next_prev_hidden = out[:, -1:]
+
+        out = self.to_out(out)
+
+        if not return_next_prev_hidden:
+            return out
+
+        return out, next_prev_hidden
+
+
+def exists(v):
+    return v is not None
+
+
+def default(v, d):
+    return v if exists(v) else d
+
+
+def heinsen_associative_scan_log(
+        log_coefficients: torch.Tensor,
+        log_values: torch.Tensor
+) -> torch.Tensor:
+    a_star: torch.Tensor = log_coefficients.cumsum(dim=1)
+    log_h0_plus_b_star: torch.Tensor = \
+        (log_values - a_star).logcumsumexp(dim=1)
+
+    log_h: torch.Tensor = a_star + log_h0_plus_b_star
+
+    return log_h.exp()
+
+
+def g(x):
+    return torch.where(x >= 0, x + 0.5, x.sigmoid())
+
+
+def log_g(x):
+    return torch.where(x >= 0, (F.relu(x) + 0.5).log(), -F.softplus(-x))
---
+++ llm/llm/architecture/tmyts/tymyts_llm.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+
+from collections import OrderedDict
+
+from llm.llm.architecture.tmyts.min_gru import minGRU
+
+
+class TymysLLM(nn.Module):
+    def __init__(self,
+                 hidden_dim: int,
+                 seq_length: int,
+                 vocabulary_size: int,
+                 dropout_rate: float,
+                 num_heads: int,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hidden_size: int = hidden_dim
+        self.seq_length: int = seq_length
+        self.vocabulary_size: int = vocabulary_size
+        self.dropout_rate: float = dropout_rate
+        self.num_heads: int = num_heads
+
+        self.embeddings: nn.Embedding = nn.Embedding(
+            num_embeddings=self.vocabulary_size,
+            embedding_dim=self.hidden_size
+        )
+
+        self.minGRU: minGRU = minGRU(dim=self.hidden_size)
+
+        self.drop_embedding: nn.Dropout = nn.Dropout(self.dropout_rate)
+        self.drop_rnn: nn.Dropout = nn.Dropout(self.dropout_rate)
+        self.drop_att: nn.Dropout = nn.Dropout(self.dropout_rate)
+        self.drop_mental_model: nn.Dropout = nn.Dropout(self.dropout_rate)
+        self.drop_mental_mlp: nn.Dropout = nn.Dropout(self.dropout_rate)
+        self.drop_grammar_mlp: nn.Dropout = nn.Dropout(self.dropout_rate)
+
+        self.norm0: nn.LayerNorm = nn.LayerNorm(self.dropout_rate)
+        self.norm1: nn.LayerNorm = nn.LayerNorm(self.dropout_rate)
+        self.norm2: nn.LayerNorm = nn.LayerNorm(self.hidden_size)
+        self.norm3: nn.LayerNorm = nn.LayerNorm(self.hidden_size)
+        self.norm4: nn.LayerNorm = nn.LayerNorm(self.hidden_size)
+
+        self.multihead_attn = torch.nn.MultiheadAttention(
+            embed_dim=self.hidden_size,
+            num_heads=self.num_heads,
+            dropout=self.dropout_rate,
+            bias=False,
+            batch_first=True
+            )
+
+        self.mental_model = nn.Sequential(OrderedDict([
+            ('linear_1', nn.Linear(self.hidden_size, self.hidden_size)),
+            ('act_1', nn.GELU()),
+            ('linear_2', nn.Linear(self.hidden_size, self.hidden_size)),
+            ('act_2', nn.GELU()),
+            ('linear_3', nn.Linear(self.hidden_size, self.hidden_size)),
+            ('act_3', nn.GELU()),
+            ('linear_4', nn.Linear(self.hidden_size, self.hidden_size)),
+            ('act_4', nn.GELU()),
+        ]))
+
+        self.grammar_model = nn.Sequential(OrderedDict([
+            ('linear_1', nn.Linear(
+                self.hidden_size, self.vocabulary_size
+            )),
+            ('act_1', nn.GELU()),
+            ('linear_2', nn.Linear(
+                self.vocabulary_size, self.vocabulary_size
+            )),
+            ('act_2', nn.GELU()),
+            ('linear_3', nn.Linear(
+                self.vocabulary_size, self.vocabulary_size
+            )),
+            ('act_3', nn.GELU()),
+            ('linear_4', nn.Linear(
+                self.vocabulary_size, self.hidden_size
+            )),
+            ('act_4', nn.GELU()),
+        ]))
+
+        self.output = nn.Sequential(OrderedDict([
+            ('linear_1', nn.Linear(
+                self.hidden_size, self.vocabulary_size
+            )),
+            ('act_4', nn.GELU()),
+            ('linear_2', nn.Linear(
+                self.vocabulary_size, self.vocabulary_size
+            )),
+            ('out_act', nn.Sigmoid()),
+        ]))
+
+    def forward(
+            self,
+            X: torch.Tensor
+            ) -> torch.Tensor:
+        # validate input:
+        # shape and dtype
+
+        # comments
+        shortcut: torch.Tensor = X
+        X: torch.Tensor = self.embeddings(X)
+        X = self.drop_embedding(X)
+
+        X = self.norm0(X)
+        output, hidden_state = self.minGRU(X, return_next_prev_hidden=True)
+        X = output + hidden_state
+        X = self.drop_rnn(X)
+        X = X + shortcut
+
+        # multihead
+        X = self.norm1(X)
+        X = self.multihead_attn(X, X, X)
+        X = self.drop_att(X)
+        X = X + shortcut
+
+        # mental model
+        X_m: torch.Tensor = self.norm2(X)
+        X_m = self.mental_model(X_m)
+        X_m = self.drop_mental_mlp(X_m)
+        X_m = X_m + shortcut
+
+        # grammar model
+        X_g: torch.Tensor = self.norm3(X)
+        X_g = self.grammar_model(X_g)
+        X_g = self.drop_grammar_mlp(X_g)
+        X_g = X_g + shortcut
+
+        # union
+        X = X_m + X_g
+
+        # output
+        X = self.norm4(X)
+        X = self.output(X)
+
+        return X
